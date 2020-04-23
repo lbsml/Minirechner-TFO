b2184e16e84d5fa02e6c6062e79f7cc1d918e808
Michael Lobis <62984614+lbsml@users.noreply.github.com>
2020-04-23 11:00:00 +0200

    Menüstruktur_V10

---
+++ Minirechner/Minirechner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Minirechner
+{
+    class Program
+    {
+        
+        
+
+        static void Main(string[] args)
+        {
+            int Zahl1 = 0;
+            int Zahl2 = 0;
+            int Erg = 0;
+
+
+
+            Console.WriteLine("Minirechner-TFO");
+            Console.WriteLine();
+            Console.WriteLine("Bitte geben Sie die erste Zahl ein: ");
+            Zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bitte geben Sie die zweite Zahl ein: ");
+            Zahl2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Wählen Sie die gewünschte Grundrechnungsart aus!);
+            Console.WriteLine("1    Addieren");
+            Console.WriteLine("2    Subtrahieren");
+            Console.WriteLine("3    Multiplizieren");
+            Console.WriteLine("4    Dividieren");
+            Console.ReadKey();
+        }
+    }
+}
